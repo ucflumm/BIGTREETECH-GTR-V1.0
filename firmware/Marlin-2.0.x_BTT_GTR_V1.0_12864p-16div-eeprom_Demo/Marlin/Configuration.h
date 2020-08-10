7bbc493f0e345ab2af8ed3b7da7cd37e7c58fc84
Uwe Flumm <ucflumm@gmail.com>
2020-08-10 13:36:47 +1000

    Update Configuration.h

--- firmware/Marlin-2.0.x_BTT_GTR_V1.0_12864p-16div-eeprom_Demo/Marlin/Configuration.h
+++ firmware/Marlin-2.0.x_BTT_GTR_V1.0_12864p-16div-eeprom_Demo/Marlin/Configuration.h
@@ -1092,8 +1092,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 330
+#define Y_BED_SIZE 330
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1101,7 +1101,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 400
 
 /**
  * Software Endstops
