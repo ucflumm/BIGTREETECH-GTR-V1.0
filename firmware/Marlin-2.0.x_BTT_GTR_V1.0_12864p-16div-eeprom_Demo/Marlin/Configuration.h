efc0c61194409ec9b4443df5f0769bf7a7507539
Uwe Flumm <ucflumm@gmail.com>
2020-08-10 18:50:46 +1000

    Update Configuration.h

--- firmware/Marlin-2.0.x_BTT_GTR_V1.0_12864p-16div-eeprom_Demo/Marlin/Configuration.h
+++ firmware/Marlin-2.0.x_BTT_GTR_V1.0_12864p-16div-eeprom_Demo/Marlin/Configuration.h
@@ -1096,7 +1096,7 @@
 #define Y_BED_SIZE 320
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -20s
+#define X_MIN_POS -20
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
